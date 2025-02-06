8db08c9f171e7fb7cd94271a759b5fb3f6bbdc4d
Abdulhamit4216 <abdulhamittiki44@gmail.com>
2025-02-06 18:32:09 +0300

    Ana sayfa class'ı oluşturuldu.

---
+++ JavaSwingUygulamasi/src/main/java/org/example/HomePage.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import javax.swing.*;
+
+public class HomePage {
+    JFrame frame = new JFrame();
+
+    HomePage(){
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+
+        frame.setVisible(true);
+    }
+}
