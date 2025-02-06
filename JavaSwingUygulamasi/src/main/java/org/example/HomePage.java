548bf993c160bd0bf189d2b0f117ffde0b9aa8d1
Abdulhamit4216 <abdulhamittiki44@gmail.com>
2025-02-06 18:39:22 +0300

    Frame profil butonu eklendi.

--- JavaSwingUygulamasi/src/main/java/org/example/HomePage.java
+++ JavaSwingUygulamasi/src/main/java/org/example/HomePage.java
@@ -4,14 +4,16 @@ import javax.swing.*;
 
 public class HomePage {
     JFrame frame = new JFrame();
+    JButton profileButton = new JButton("Profili Göster");
 
     HomePage(){
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
 
-
-
-
+        profileButton.setBounds(200,200,100,50);
+        profileButton.setFocusable(false);
+        frame.add(profileButton);
 
         frame.setVisible(true);
     }
